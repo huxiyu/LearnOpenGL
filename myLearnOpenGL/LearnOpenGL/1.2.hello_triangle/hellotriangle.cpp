4328815b81c75182f68b1277c6d165ca53c6358a
huxiyu <hu_xi_yu@qq.com>
2016-02-24 13:42:20 +0800

    完成1.2.hello_triangle
    分两种绘制：
    1. VAO VBO
    2. VAO VBO EBO
    注意：OpenGL核心模式版要求我们使用VAO。
    这样它就能知道对我们的顶点输入做些什么。如果我们绑定VAO失败，OpenGL会拒绝绘制任何东西。

--- myLearnOpenGL/LearnOpenGL/1.2.hello_triangle/hellotriangle.cpp
+++ myLearnOpenGL/LearnOpenGL/1.2.hello_triangle/hellotriangle.cpp
@@ -10,6 +10,18 @@
 // Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
+// VAO VBO glDrawArrays
+void vao_vbo_glDrawArrays();
+
+// VAO EBO VBO glDrawElements
+void vao_vbo_ebo_glDrawElements();
+
+// GLFW window
+GLFWwindow* window;
+
+// shaderProgram
+GLint shaderProgram;
+
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
 
@@ -42,7 +54,7 @@ int main()
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	// Create a GLFWwindow object that we can use for GLFW's functions
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "hello triangle", nullptr, nullptr);
+	window = glfwCreateWindow(WIDTH, HEIGHT, "hello triangle", nullptr, nullptr);
 	if (window == nullptr)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -93,7 +105,7 @@ int main()
 		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
 	// Link shaders
-	GLint shaderProgram = glCreateProgram();
+	shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
@@ -107,7 +119,18 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	// vao_vbo_glDrawArrays
+	//vao_vbo_glDrawArrays();
+	vao_vbo_ebo_glDrawElements();
 
+	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glfwTerminate();
+	return 0;
+}
+
+void vao_vbo_glDrawArrays()
+{
+	
 	/* Set up vertex data (and buffer(s)) and attribute pointers */
 	GLfloat vertices[] = {
 		-0.5f, -0.5f, 0.0f,
@@ -129,7 +152,8 @@ int main()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
-	//4. 解绑VAO
+	//4. 解绑 VBO VAO
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
 	// Game loop
@@ -154,14 +178,80 @@ int main()
 	}
 
 	// Properly de-allocate all resources once they've outlived their purpose
+	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-
-	// Terminate GLFW, clearing any resources allocated by GLFW.
-	glfwTerminate();
-	return 0;
+	
 }
 
+void vao_vbo_ebo_glDrawElements()
+{
+		/* Set up vertex data (and buffer(s)) and attribute pointers */
+	GLfloat vertices[] = {
+		0.5f, 0.5f, 0.0f,   // 右上角
+		0.5f, -0.5f, 0.0f,  // 右下角
+		-0.5f, -0.5f, 0.0f, // 左下角
+		-0.5f, 0.5f, 0.0f   // 左上角
+	};
 
+	GLuint indices[] = { // 起始于0!
+		0, 1, 3, // 第一个三角形
+		1, 2, 3  // 第二个三角形
+	};
+
+	GLuint VAO, VBO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	// 1. 绑定VAO
+	glBindVertexArray(VAO);
+
+	// 2. 把我们的顶点数组复制到一个顶点缓冲中，提供给OpenGL使用
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// 2. 复制我们的索引数组到一个索引缓冲中，提供给OpenGL使用
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// 3. 设置顶点属性指针
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	//4. 解绑 VAO VBO EBO
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	// Uncommenting this call will result in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	// Game loop
+	while(!glfwWindowShouldClose(window))
+	{
+		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+		glfwPollEvents();
+
+		// Render
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Draw our first triangle
+		glUseProgram(shaderProgram);
+
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		// Swap the screen buffers
+		glfwSwapBuffers(window);
+	}
+
+	// Properly de-allocate all resources once they've outlived their purpose
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+}
 
 
 // Is called whenever a key is pressed/realeased via GLFW
